a615c7e9edac3e02c46b3937bcee697e29ae5a70
hiro501 <hiro0501saito@gmail.com>
2024-03-26 00:44:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :kanji_last_name, presence: true
+  validates :kanji_first_name, presence: true
+  validates :katakana_last_name, presence: true
+  validates :katakana_first_name, presence: true
+  validates :birth_date, presence: true
 end
