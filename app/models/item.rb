1ba21ce7781e3b6037e011bb2bf848345e584f2e
hiro501 <hiro0501saito@gmail.com>
2024-03-30 00:17:27 +0900

    商品登録画面へのルーティングの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
- 
+  belongs_to :user
 
+  validates :price, presence: true
+  validates :item_name, presence: true
+  validates :description_id, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :image, presence: true
 end
