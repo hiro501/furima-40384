a0247587bf8bc1b2f606e1428bc591da9e416411
hiro501 <hiro0501saito@gmail.com>
2024-03-30 01:00:03 +0900

    Active Storageの導入と設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,14 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
-    @user = User.new
+    @item = Item.new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:price, :item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :price, presence: true
   validates :item_name, presence: true
-  validates :description_id, presence: true
+  validates :description, presence: true
   validates :category_id, presence: true
   validates :condition_id, presence: true
   validates :shipping_fee_id, presence: true
