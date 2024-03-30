3569d1040b00f15340a653dea53068b0223db79e
hiro501 <hiro0501saito@gmail.com>
2024-03-30 23:56:48 +0900

    商品状態の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
