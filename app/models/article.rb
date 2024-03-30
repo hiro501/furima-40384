32ab690593cc04398a1b7b41834ecb68b854e6fa
hiro501 <hiro0501saito@gmail.com>
2024-03-31 01:20:35 +0900

    配送までの日数の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,11 @@ class Article < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :shipping_day
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
