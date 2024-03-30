dc9edc617034fad77b7e1ce9ff71924fa4790350
hiro501 <hiro0501saito@gmail.com>
2024-03-31 01:14:09 +0900

    配送元の地域の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,10 @@ class Article < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
+  belongs_to :prefecture
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
