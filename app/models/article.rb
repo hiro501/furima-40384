6a023d0f1e1f2198d9912f2ae4d350608c29605a
hiro501 <hiro0501saito@gmail.com>
2024-03-31 01:04:27 +0900

    配送料の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,9 @@ class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_fee
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
