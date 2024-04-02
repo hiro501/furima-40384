0c1d510346393941fa283cea9faf749b5e05abee
hiro501 <hiro0501saito@gmail.com>
2024-04-02 11:12:33 +0900

    販売手数料の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,15 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:price, :item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A\d+\z/ }
   validates :item_name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :image, presence: true
 end
