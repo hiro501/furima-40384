600bcab70d2d12fa742d68cc564a9be3a46ac0bc
hiro501 <hiro0501saito@gmail.com>
2024-04-03 16:07:06 +0900

    商品出品機能の修正1回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   validates :price, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :price, format: { with: /\A\d+\z/ }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :item_name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
