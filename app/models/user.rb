a6c38de742e6c048ddb7563575d49a891c06d190
hiro501 <hiro0501saito@gmail.com>
2024-04-05 17:08:44 +0900

    マイグレーションファイルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/
 
-  validates :name, presence: true
-  validates :kanji_last_name, presence: true, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}]+\z/ }
-  validates :kanji_first_name, presence: true, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}]+\z/ }
-  validates :katakana_last_name, presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/ }
-  validates :katakana_first_name, presence: true, format: { with: /\A[\p{Katakana}ー－]+\z/ }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :name
+    validates :kanji_last_name, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}]+\z/ }
+    validates :kanji_first_name, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}]+\z/ }
+    validates :katakana_last_name, format: { with: /\A[\p{Katakana}ー－]+\z/ }
+    validates :katakana_first_name, format: { with: /\A[\p{Katakana}ー－]+\z/ }
+    validates :birth_date
+  end
   validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   has_many :items
